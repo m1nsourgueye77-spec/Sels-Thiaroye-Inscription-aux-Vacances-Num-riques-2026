3f8d68063aac3e7ff5afdcd28b1c24afcfcf148c
Africa Light Site <m1nsour.gueye77@gmail.com>
2026-07-22 18:19:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,29 @@ echeance.addEventListener("change", () => {
 
 }); // ← fermeture de addEventListener("change")
 
-const url="VOTRE_URL_APPS_SCRIPT";
+const url="https://script.google.com/macros/s/AKfycbytgwHAxbWHyRIX2zygeWZVk7bwz3NFcHIQ05Cnvi0rkfyRixRKtKoWTCr6uRHO-Idi/exec";
 
 document.getElementById("form").addEventListener("submit",function(e){
 
 e.preventDefault();
 
+
+if(
+!this.informatique.checked &&
+!this.permis.checked &&
+!this.coran.checked
+){
+
+alert("Veuillez choisir au moins une formation");
+
+return;
+
+}
+
+
 const data={
 
+    
 prenoms:this.prenoms.value,
 
 nom:this.nom.value,
@@ -49,6 +64,8 @@ engagement:this.engagement.checked?"Oui":"Non"
 
 };
 
+
+
 fetch(url,{
 method:"POST",
 body:JSON.stringify(data)
@@ -67,7 +84,8 @@ this.reset();
 
 .catch(()=>{
 
-document.getElementById("message").innerHTML="Erreur.";
+document.getElementById("message").innerHTML=
+"❌ Erreur.";
 
 });
 
